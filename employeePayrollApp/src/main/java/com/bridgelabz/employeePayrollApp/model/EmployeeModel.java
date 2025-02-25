7f61bb2e4bdf52eddafd38d5f3dac29649770140
SurveshBajaj <Sarveshbajaj9@gmail.com>
2025-02-25 16:58:45 +0530

    [Survesh Bajaj]- Add. Introducing DTO and Model to Employee Payroll App

---
+++ employeePayrollApp/src/main/java/com/bridgelabz/employeePayrollApp/model/EmployeeModel.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.employeePayrollApp.model;
+
+
+import com.bridgelabz.employeePayrollApp.DTO.EmployeeDTO;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class EmployeeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double salary;
+
+
+    public EmployeeModel( EmployeeDTO employeePayrollDTO) {
+        this.name = employeePayrollDTO.getName();
+        this.salary = employeePayrollDTO.getSalary();
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
